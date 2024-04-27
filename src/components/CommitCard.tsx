650768bff946cb8aaefab680a25b87a29bdd6164
Mancopp <mancoppor@gmail.com>
2024-04-27 19:23:31 +0200

    Add branch styling calculation. Add more styles for commit-card

--- src/components/CommitCard.tsx
+++ src/components/CommitCard.tsx
@@ -1,31 +1,30 @@
 import { Commit } from "./CommitsGraph";
 import "./CommitCard.scss";
 
-const branchColors = {
-  UNI: "blue",
-  JOB: "red",
-  PER: "green",
-};
-
 export const CommitCard = (props: {
   commit: Commit;
   isNextCommitSameBranch: boolean;
 }) => {
   const commit = props.commit;
-  const style = {
-    "--color-primary": branchColors[commit.branch] || "black",
+  const overrideStyles = {
+    "--color-primary": commit.branch.color,
+    "--branch-offset-width": `${commit.branch.order * 30}px`,
   } as React.CSSProperties;
-
   return (
-    <div
-      className={`commit-card ${props.isNextCommitSameBranch ? "last" : ""}`}
-      style={style}
-    >
-      <div className="commit-card__content">
-        <h2>{commit.title}</h2>
-        <p>{commit.branch}</p>
-        <p>{props.isNextCommitSameBranch.toString()}</p>
+    <div className={"commit-card"} style={overrideStyles}>
+      <div className="commit-card__rect">
+        <div className="content">
+          <h3 className="title">{commit.title}</h3>
+          <p className="subtitle">{commit.subtitle}</p>
+          <p className="description">{commit.description}</p>
+        </div>
+        <div className="image"></div>
       </div>
+      <div
+        className={`commit-card__stripe ${
+          props.isNextCommitSameBranch ? "connect-to-prev" : ""
+        }`}
+      ></div>
     </div>
   );
 };
--- src/components/CommitsGraph.tsx
+++ src/components/CommitsGraph.tsx
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import { CommitCard } from "./CommitCard";
 import "./CommitsGraph.scss";
 
-enum Branches {
-  UNIVERSITY = "UNI",
-  JOB = "JOB",
-  PERSONAL = "PER",
-}
+type Branch = {
+  name: string;
+  color: string;
+  order: number;
+  lastCommitId?: number;
+};
 
 export type Commit = {
   title: string;
-  branch: Branches;
+  subtitle?: string;
+  description?: string;
+  branch: Branch;
 };
 
 export const CommitsGraph = () => {
@@ -43,28 +46,54 @@ export const CommitsGraph = () => {
     getElementsInArea();
   }, []);
 
+  const branches: Branch[] = [
+    {
+      name: "university",
+      color: "#449DD1",
+      order: 3,
+    },
+    {
+      name: "job",
+      color: "#D1603D",
+      order: 2,
+    },
+    {
+      name: "personal",
+      color: "#EEC643",
+      order: 1,
+    },
+  ];
+
   const commits: Commit[] = [
     {
-      title: "Uni blah",
-      branch: Branches.UNIVERSITY,
+      title: "PROJECT FLOW",
+      subtitle: "Sep 2023 - Present",
+      description:
+        "Adventure game I am working on while building framework based on Godot 4. Creating architecture that allows future users not to worry about handling complex state which is often present in games of point-and-click adventure genre. During this project I have learned a lot about caching and general lower level optimization.",
+      branch: branches[0],
     },
     {
       title: "Uni blah blah",
-      branch: Branches.UNIVERSITY,
+      branch: branches[0],
     },
     {
       title: "Job blah",
-      branch: Branches.JOB,
+      branch: branches[1],
     },
     {
       title: "Uni blah blah blah",
-      branch: Branches.UNIVERSITY,
+      branch: branches[0],
     },
     {
       title: "Personal blah",
-      branch: Branches.PERSONAL,
+      branch: branches[2],
     },
   ];
+
+  commits.forEach((commit, index) => {
+    const b = commit.branch;
+    b.lastCommitId = index;
+  });
 
   return (
     <div className="commits-graph">
@@ -79,7 +108,33 @@ export const CommitsGraph = () => {
           }
         />
       ))}
-      <div className="branch-line-uni"></div>
+      {branches
+        .sort((a, b) => b.lastCommitId - a.lastCommitId)
+        .map((branch, index) => (
+          <BranchLine key={index} branch={branch} />
+        ))}
     </div>
+  );
+};
+
+const BranchLine = (props: { branch: Branch }) => {
+  const calculateBranchHeight = (
+    lastCommitId: number,
+    commitHeight: number,
+    gap: number = 0,
+    offsetTop: number = 0
+  ) => (lastCommitId + 1) * (commitHeight + gap) + 150 + offsetTop;
+
+  const style = {
+    borderLeft: `10px solid ${props.branch.color}`,
+    borderBottom: `10px solid ${props.branch.color}`,
+    width: `${props.branch.order * 30}px`,
+    height:
+      props.branch.lastCommitId &&
+      ` ${calculateBranchHeight(props.branch.lastCommitId, 400, 40, 100)}px`,
+  } as React.CSSProperties;
+
+  return (
+    <div className={`branch-line-${props.branch.name}`} style={style}></div>
   );
 };
