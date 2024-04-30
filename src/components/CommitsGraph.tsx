ef1f328362d9e2fe6645e9cbaa1f460a342a10bb
Mancopp <mancoppor@gmail.com>
2024-04-30 08:04:06 +0200

    Code cleanup

--- src/components/CommitsGraph.tsx
+++ src/components/CommitsGraph.tsx
@@ -5,44 +5,12 @@ import { commits } from "../data/commits";
 import { Branch, branches } from "../data/branches";
 
 export const CommitsGraph = () => {
-  // useEffect(() => {
-  //   const getElementsInArea = () => {
-  //     const viewportHeight = document.documentElement.clientHeight;
-
-  //     const handleScroll = () => {
-  //       const elements = document.querySelectorAll(".commit-card");
-  //       elements.forEach((elm) => {
-  //         const pos = elm.getBoundingClientRect();
-  //         const topPerc = (pos.top / viewportHeight) * 100;
-  //         const bottomPerc = (pos.bottom / viewportHeight) * 100;
-  //         const middle = (topPerc + bottomPerc) / 2;
-  //         const inViewport = middle > 40 && middle < 60;
-
-  //         elm.classList.toggle("highlight", inViewport);
-  //       });
-  //     };
-
-  //     window.addEventListener("scroll", handleScroll);
-  //     window.addEventListener("resize", handleScroll);
-
-  //     return () => {
-  //       window.removeEventListener("scroll", handleScroll);
-  //       window.removeEventListener("resize", handleScroll);
-  //     };
-  //   };
-
-  //   getElementsInArea();
-  // }, []);
-
-  // Assign lastCommitId to branches
   commits.forEach((commit, index) => {
     commit.branch.lastCommitId = index;
   });
 
-  // Sort branches by lastCommitId in ascending order
   branches.sort((a, b) => (b.lastCommitId ?? 0) - (a.lastCommitId ?? 0));
 
-  // Assign order to branches based on lastCommitId starting from 1
   branches.forEach((branch, index) => {
     branch.order = index + 1;
   });
